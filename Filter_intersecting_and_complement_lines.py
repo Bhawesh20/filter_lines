42d2f9d01cb62fe86996187005cffd1d6e1beaf8
Bhawesh20 <bhawesh.agarwal20@gmail.com>
2017-05-28 12:58:23 +0530

    file

--- Filter_intersecting_and_complement_lines.py
+++ Filter_intersecting_and_complement_lines.py
@@ -3,9 +3,23 @@ f2 = open('file2.txt')
 outfile_i = open('outfile_intersection.txt', 'w')
 outfile_c = open('outfile_complement.txt', 'w')
 with f1 as f1, f2 as f2, outfile_i as outfile_i, outfile_c as outfile_c:
-    for line1, line2 in zip(f1, f2):
-        if line1 == line2:
+    for line1 in f1:
+        if line1 in f2:
             print(line1, end='', file=outfile_i)
-        else:
-            print('File 1 -->', line1, end='',file=outfile_c)
-            print('File 2 -->', line2, end='',file=outfile_c)
+        f2.seek(0)
+        
+    f1.seek(0)
+    f2.seek(0)
+    for line1 in f1:
+        if line1 not in f2:
+            print(line1,end='',file=outfile_c)
+        f2.seek(0)
+        
+    f1.seek(0)
+    f2.seek(0)
+    outfile_i.seek(0)
+    for line2 in f2:
+        if line2 not in f1:
+            print(line2,end='',file=outfile_c)
+        f1.seek(0)
+                
