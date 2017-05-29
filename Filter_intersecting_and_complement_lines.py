b7274bac72d9b65c2769bb22990233314bbc53c6
Bhawesh20 <bhawesh.agarwal20@gmail.com>
2017-05-29 14:27:33 +0530

    Updated

--- Filter_intersecting_and_complement_lines.py
+++ Filter_intersecting_and_complement_lines.py
@@ -1,25 +1,22 @@
-f1 = open('file1.txt')
-f2 = open('file2.txt')
+from itertools import chain
+f1 = open('file1.txt','r')
+f2 = open('file2.txt','r')
 outfile_i = open('outfile_intersection.txt', 'w')
 outfile_c = open('outfile_complement.txt', 'w')
 with f1 as f1, f2 as f2, outfile_i as outfile_i, outfile_c as outfile_c:
-    for line1 in f1:
-        if line1 in f2:
-            print(line1, end='', file=outfile_i)
-        f2.seek(0)
-        
+    i = 0
+    l = len(f1.readlines())
     f1.seek(0)
-    f2.seek(0)
-    for line1 in f1:
-        if line1 not in f2:
-            print(line1,end='',file=outfile_c)
-        f2.seek(0)
-        
-    f1.seek(0)
-    f2.seek(0)
-    outfile_i.seek(0)
-    for line2 in f2:
-        if line2 not in f1:
-            print(line2,end='',file=outfile_c)
-        f1.seek(0)
-                
+    for line1 in chain(f1,f2):
+        if i<l:
+            if line1 in f2:
+                print(line1, end='', file=outfile_i)
+            else:
+                print(line1,end='',file=outfile_c)
+            f2.seek(0)
+        else:
+            f1.seek(0)
+            if line1 not in f1:
+                print(line1,end='',file=outfile_c)
+        i = i+1
+            
